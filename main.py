caf7aab67660dd59f3ee0386c6a3e59880d4584e
Mareczekol <130496823+Mareczekol@users.noreply.github.com>
2023-07-03 17:12:21 +0200

    Code reviewed, changes has been added.

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
-weight_count = 0
+
 package_weight = 20
-packages_count = 1
-empty_kilograms = [[]]
-
+elements = []
+packages = [0];
+ 
 print("Ile elementów chcesz wysłać?: ")
-parcel_count = int(input())
+elements_count = int(input())
 
-for element in range(1, parcel_count + 1):
+for element in range(1, elements_count + 1):
     print(f"Podaj wagę elementu {element}")
     element_weight = int(input())
     if element_weight > 10 or element_weight < 1:
-        weight_count += element_weight
-        packages_count += 1
-        empty_kilograms.append([])
-        empty_kilograms[packages_count - 1].append(package_weight -
-                                                   weight_count)
         break
-    while weight_count + element_weight > package_weight:
-        packages_count += 1
-        empty_kilograms.append([])
-        empty_kilograms[packages_count - 1].append(
-            package_weight - weight_count)
-        break
-    if weight_count + element_weight <= package_weight:
-        weight_count += element_weight
-        empty_kilograms[packages_count - 1].append(package_weight -
-                                                   weight_count)
+    elements.append(element_weight)
+ 
+
+for element in range(0, len(elements)):
+    element_weight = elements[element]
+    weigth = packages[len(packages) - 1]
+    if element_weight + weigth > package_weight:
+        packages.append(element_weight)
     else:
-        weight_count += element_weight
-    if element_weight == parcel_count:
-        empty_kilograms[packages_count - 1].append(package_weight -
-                                                   weight_count)
-
-max_empty_kilograms = max([max(empty_kgs) for empty_kgs in empty_kilograms])
-max_empty_package = [i+1 for i, empty_kgs in enumerate(empty_kilograms) if
-                     max(empty_kgs) == max_empty_kilograms][0]
-max_empty_weight = max_empty_kilograms if max_empty_kilograms > 0 else 0
-
-print(f'1. Ilość wysłanych paczek to: {packages_count}')
-print(f'2. Ilość wysłanych kilogramów: {weight_count}')
-print(f'3. Ilość wysłanych pustych kilogramów: '
-      f'{packages_count * 20 - weight_count}')
+        packages[len(packages) - 1] += element_weight
+   
+min_weigth = min(packages) 
+max_empty_index = packages.index(min_weigth)+1
+max_empty_weight = package_weight - min_weigth
+ 
+# suma pustych kgs
+sum_empty_kgs = len(packages) * package_weight - sum(packages)
+ 
+print(f'1. Ilosc wyslanych paczek to: {len(packages)} o kilogramach {packages}')
+print(f'2. Ilość wysłanych kilogramów: {sum(packages)}')
+print(f'3. Ilość wysłanych pustych kilogramów: {sum_empty_kgs}')
 print(f'4. Paczka z największą ilością pustych kilogramów: '
-      f'{max_empty_package} z {max_empty_weight} pustymi kilogramami')
-# ostatnia komenda print - zawsze pokazuje paczkę 1 z 10 pustymi kg - niestety
-# nie wiem jak to prawidłowo napisać. Poproszę o wskazówki
+      f'{max_empty_index} z {max_empty_weight} pustymi kilogramami')
